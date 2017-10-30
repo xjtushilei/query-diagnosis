81cfcfa95b986b6c230d087f6a822b29c8ba7322
xjtushilei <xjtushilei@foxmail.com>
2017-10-30 10:10:08 +0800

    init

---
+++ src/main/java/com/xjtushilei/querydiagnosis/QueryDiagnosisApplication.java
@@ -0,0 +1,12 @@
+package com.xjtushilei.querydiagnosis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QueryDiagnosisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QueryDiagnosisApplication.class, args);
+    }
+}
